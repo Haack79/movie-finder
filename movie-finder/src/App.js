af2d90b661434469becba2c26be05ab167f9716f
Brian Haack <haack79@hotmail.com>
2020-07-30 09:47:20 -0700

    made search component

--- movie-finder/src/App.js
+++ movie-finder/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import SearchMovie from "./searchMovie"; 
 // import './App.css';
 import "./styles.css"; 
 function App() {
   return (
     <div className="container">
-      <header className="App-header">whazzzupp</header>
-      <p>Hello Friends!</p>
-      <h1 className="container">React Movie Finder</h1>
+      <header className="title">whazzzupp</header>
+      <p className="title">Hello Friends!</p>
+      <h1 className="title">React Movie Finder</h1>
+      <SearchMovie />
 
     </div>
   );
