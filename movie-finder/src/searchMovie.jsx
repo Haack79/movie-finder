cd4a8e42b056a7d69627f26520c20ab27dd8fa16
Brian Haack <haack79@hotmail.com>
2020-07-31 10:10:52 -0700

    updated component to show data received from api

--- movie-finder/src/searchMovie.jsx
+++ movie-finder/src/searchMovie.jsx
@@ -5,18 +5,18 @@ const SearchMovie = () => {
     const [movies, setMovies] = useState([]); 
     const searchMovies = async (e) => {
         e.preventDefault(); 
-        const url = `https://api.themoviedb.org/3/movie/550?api_key=d1e6e6ef719057324e1cc71756ebd601?language=en-US$query=${query}$page=1$
-        include_adult=false`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=d1e6e6ef719057324e1cc71756ebd601&language=en-US&query=${query}&page=1&include_adult=false`
         try {
             const res = await fetch(url);
             const data = await res.json(); 
             setMovies(data.results); 
+            console.log(movies); 
         } catch (err) {
             console.error(err); 
         }
     }
     return (
-        <div>
+        <React.Fragment>
             <form className="form" onSubmit={searchMovies}>
                 <label className="label" htmlFor="query">Movie Name</label>
                 <input className="input" type="text" name="query" placeholder="i.e. Jurrasic Park"
@@ -24,7 +24,17 @@ const SearchMovie = () => {
                 />
                 <button className="button" type="submit">Search</button>
             </form>
-        </div>
+            <div className="card-list">
+                {movies.map(movie => (
+                    <div className="card">
+                        <img className="card--image"
+                        src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                        alt={movie.title + ' poster'}
+                        />
+                    </div>
+                ))}
+            </div>
+        </React.Fragment>
     )
 }
 
